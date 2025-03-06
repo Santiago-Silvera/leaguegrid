318a664fc987cc543cf39662a0fead20b35b5cf3
Santiago-Silvera <ssilvera@correo.um.edu.uy>
2025-03-05 21:35:59 -0300

    Changed to flask and cleaned up

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+//const fillGrid = async () => {
+//    const gridContainer = document.getElementById("grid");
+//    const gridSize = 3; // 3 (champs) + 1 (question)
+//    for (let row = 0; row < gridSize; row++) {
+//        for (let col = 0; col < gridSize; col++) {
+//            let div = document.createElement("div");
+//            div.className = "bg-gray-700 w-32 h-32 transition-colors duration-200 hover:bg-gray-500";
+//
+//            gridContainer.appendChild(div);
+//        }
+//    }
+//};
+//
+//document.addEventListener("DOMContentLoaded", fillGrid);
