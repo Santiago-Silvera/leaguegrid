724405fc277c1ab8200c96bae92592f310db68f2
Santiago-Silvera <ssilvera@correo.um.edu.uy>
2025-03-06 05:42:40 -0300

    Added showing random strings

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, jsonify
+import random
+import string
 
 app = Flask(__name__)
 
 @app.route("/")
 def serve_html():
-    grid_data = [[f"" for col in range(3)] for row in range(3)]
-    return render_template('index.html', grid=grid_data)
+    num_rows = 3
+    num_col = 3
+    grid_data = [[f"" for col in range(num_col)] for row in range(num_rows)]
+    top_questions = [random_string() for _ in range(num_col)]  # 3 random top questions
+    left_questions = [random_string() for _ in range(num_rows)]  # 3 random left questions
+    return render_template('index.html', grid=grid_data, top_questions=top_questions, left_questions=left_questions, zip=zip)
+
+def random_string(length=8):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 if __name__ == "__main__":
     app.run(debug=True)
